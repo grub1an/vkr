d43909a644a226120168865f3b373de2250f1732
Vitaly <vitalik.myasoediv@yandex.ru>
2022-10-03 19:27:07 +0300

    Простая аутентификация

---
+++ server/src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('auth')
+  auth(): Promise<any> {
+    return this.authService.validateUser('john', 'changeme');
+  }
+}
